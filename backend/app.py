04d9173c45820338da67edb35f8b95933dee3ddf
Nadun Perera <nadun.perera@me.com>
2019-05-27 15:43:44 +1000

    small db name change

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "stores"
+app.config["MONGO_DBNAME"] = "tarantula_service"
 app.config["MONGO_URI"] = "mongodb://localhost:27017/tarantula_service"
 
 mongo = PyMongo(app)
