ab5f230ed90bc1f6545b0ab4053699436e9f3479
Nadun Perera <nadun.perera@me.com>
2019-05-27 15:40:08 +1000

    added first route to list all stores

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,23 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "stores"
-app.config["MONGO_URI"] = "mongod://localhost:27017/tarantula_service"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tarantula_service"
 
 mongo = PyMongo(app)
 
+
+@app.route("/stores", methods=["GET"])
+def get_stores():
+    stores = mongo.db.stores
+
+    output = []
+
+    for q in stores.find():
+        q.pop("_id")
+        output.append(q)
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
